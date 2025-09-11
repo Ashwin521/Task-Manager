6e1607c6e62b20f7fc38303cedb4ff617e9fd099
Ashwin521 <141605960+Ashwin521@users.noreply.github.com>
2025-09-11 15:46:30 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,38 @@
-import "../server"
+// import "../server"
 
-import { app } from "../server";
+// import { app } from "../server";
 
-export default app;
+// export default app;
+
+
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "../../config/db.js";
+import authRoutes from "../../routes/authRoutes.js";
+import taskRoutes from "../../routes/taskRoutes.js";
+import serverless from "serverless-http";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://task-manager-frontend-mu-five.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ msg: "Server Error" });
+});
+
+export default serverless(app);
